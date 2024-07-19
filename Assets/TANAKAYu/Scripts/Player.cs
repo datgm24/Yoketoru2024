55eed6b8138b00eda919489256d44b89aeae8807
YuTanaka <tanakaedu@users.noreply.github.com>
2024-07-19 14:10:02 +0900

    マウスでプレイヤーを操作

--- Assets/TANAKAYu/Scripts/Player.cs
+++ Assets/TANAKAYu/Scripts/Player.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Tooltip("速さ"), SerializeField]
+    float speed = 20;
+
     float cameraDistance = 0;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -12,13 +21,27 @@ public class Player : MonoBehaviour
         cameraDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
     }
 
-    // Update is called once per frame
-    void Update()
+    // FixedUpdate = 物理処理をするための固定更新処理
+    void FixedUpdate()
     {
         var mp = Input.mousePosition;
         mp.z = cameraDistance;
         var wp = Camera.main.ScreenToWorldPoint(mp);
 
-        Debug.Log(wp);
+        var to = wp - rb.position;
+        if (to.magnitude < 0.01f)
+        {
+            rb.velocity = Vector3.zero;
+        }
+        else
+        {
+            float step = speed * Time.deltaTime;
+            float dist = Mathf.Min(to.magnitude, step);
+            float v = dist / Time.deltaTime;
+            rb.velocity = v * to.normalized;
+        }
+
+        //transform.position = wp;
+        //Debug.Log(wp);
     }
 }
