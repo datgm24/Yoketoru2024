393158dc368982cd666955493fa8b08eb15dfea7
YuTanaka <tanakaedu@users.noreply.github.com>
2024-12-12 11:34:47 +0900

    基幹クラスの定義.close #2

--- Assets/TANAKAYu/Scripts/SceneChanger.cs
+++ Assets/TANAKAYu/Scripts/SceneChanger.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// シーンを切り替える機能を提供するクラス。
+/// </summary>
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
