68f5395d48140189771b88ab040467940ab83631
YuTanaka <tanakaedu@users.noreply.github.com>
2024-12-19 15:26:58 +0900

    移動方向へ、プレイヤーを向かせる。close #23

--- Assets/TANAKAYu/Scripts/Game/CharacterMover.cs
+++ Assets/TANAKAYu/Scripts/Game/CharacterMover.cs
@@ -6,14 +6,34 @@ public class CharacterMover : MonoBehaviour, IMover
     float speed = 20;
 
     Rigidbody rb;
+    Transform pivotTransform;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        pivotTransform = transform.Find("Pivot");
     }
 
     public void Move(Vector2 move)
     {
         rb.velocity = speed * move;
+        UpdateForward(move);
+    }
+
+    /// <summary>
+    /// 前方を向く
+    /// </summary>
+    void UpdateForward(Vector2 move)
+    {
+        if (move.magnitude < 0.1f)
+        {
+            return;
+        }
+
+        // 移動があったら、その方向を向く
+        Vector3 euler = pivotTransform.eulerAngles;
+        float angle = Mathf.Atan2(-move.x, move.y) * Mathf.Rad2Deg;
+        euler.z = angle;
+        pivotTransform.eulerAngles = euler;
     }
 }
