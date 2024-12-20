199c4fa06dcc53a8a4c72c29b1f8e3d2feabbc44
YuTanaka <tanakaedu@users.noreply.github.com>
2024-12-20 12:31:10 +0900

    跳ね返り実装中(WIP)
    跳ね返らない

--- Assets/TANAKAYu/Scripts/Game/AutoMover/ReflectionMover.cs
+++ Assets/TANAKAYu/Scripts/Game/AutoMover/ReflectionMover.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ReflectionMover : MonoBehaviour
+public class ReflectionMover : MonoBehaviour, IAutoMover
 {
-    // Start is called before the first frame update
-    void Start()
+    static float MinimumSpeed => 0.01f;
+
+    [Tooltip("移動方向"), SerializeField]
+    Vector3 firstDirection = Vector3.right;
+    [Tooltip("移動速度"), SerializeField]
+    float speed = 2;
+
+    Rigidbody rb;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.velocity = speed * firstDirection.normalized;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(float delta)
     {
-        
+        if (rb.velocity.magnitude < MinimumSpeed)
+        {
+            //rb.velocity = speed * firstDirection.normalized;
+        }
+        /*
+        else
+        {
+            // 速度を維持
+            rb.velocity = speed * rb.velocity.normalized;
+        }
+        */
+    }
+
+    public void Stop()
+    {
+        rb.velocity = Vector3.zero;
     }
 }
