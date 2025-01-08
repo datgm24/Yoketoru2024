2e5deb274d6701bbc5b1eaf18b714aed05c055b7
Yu Tanaka <YRK00337@nifty.com>
2025-01-08 13:00:38 +0900

    操作をまとめるクラス

---
+++ Assets/TANAKAYu/Scripts/Game/Player/InputController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class InputController
+{
+    IInput[] inputs =
+    {
+
+    };
+
+    public void Update()
+    {
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            inputs[i].Update();
+        }
+    }
+
+    public Vector2 GetValue()
+    {
+        Vector2 move = Vector2.zero;
+
+        for (int i = 0;i<inputs.Length;i++)
+        {
+            var this_move = inputs[i].GetValue();
+            if (this_move.magnitude > move.magnitude)
+            {
+                move = this_move;
+            }
+        }
+        return move;
+    }
+}
